5dc07ea8f01818cff8b0447a1bc8164d4961bae2
Rito15 <ghdtkals15@naver.com>
2023-07-10 09:48:44 +0900

    push

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ _COMMANDS_DIR_=$(pwd) # 파일의 현재 경로
 _REGBASHRC_=${_COMMANDS_DIR_}/.nginx-custom-commands/.register-bashrc
 sudo chmod +x ${_REGBASHRC_}
 
-${_REGBASHRC_} 'export PATH="$PATH:$HOME/.rito/.nginx-custom-commands/"'
+${_REGBASHRC_} 'export PATH="$PATH:$HOME/.rito-nginx/.nginx-custom-commands/"'
 ${_REGBASHRC_} "alias nx-av='cd /etc/nginx/sites-available/'"
 ${_REGBASHRC_} "alias nx-en='cd /etc/nginx/sites-enabled/'"
 ${_REGBASHRC_} "alias nx-cd-av='cd /etc/nginx/sites-available/'"
