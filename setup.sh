0910405b4ca3ccc4a76c2461773ce174cce2e835
Rito15 <ghdtkals15@naver.com>
2023-07-10 11:00:06 +0900

    push

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 #cd "$(dirname "$0")" || exit
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
-export PATH=$(echo $PATH | sed 's|:$HOME/.rito-nginx/.nginx-custom-commands/||g')
+# export PATH=$(echo $PATH | sed 's|:$HOME/.rito-nginx/.nginx-custom-commands/||g')
+export PATH=$(echo $PATH | sed "s|$HOME/.rito-nginx/.nginx-custom-commands/||g" | sed "s|::|:|g")
 
 _COMMANDS_DIR_=$(pwd) # 파일의 현재 경로
 _REGBASHRC_=${_COMMANDS_DIR_}/.nginx-custom-commands/.register-bashrc
