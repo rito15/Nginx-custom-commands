7d25127192b18465562289984943a8a3dd5b62f9
Rito15 <ghdtkals15@naver.com>
2023-07-10 17:33:17 +0900

    modify setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+_RETURN_PATH_=$(pwd)
+
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 export PATH=$(echo $PATH | sed "s|:$HOME/.rito-nginx/.nginx-utility-commands/||g")
 
-_COMMANDS_DIR_=$(pwd) # 파일의 현재 경로
+_COMMANDS_DIR_=$(pwd) # 파일의 현재 디렉토리 절대 경로
 _REGBASHRC_=${_COMMANDS_DIR_}/.nginx-utility-commands/.register-bashrc
 
 sudo chown $USER ${_COMMANDS_DIR_}
@@ -16,3 +18,5 @@ ${_REGBASHRC_} "alias nx-cd-av='cd /etc/nginx/sites-available/'"
 ${_REGBASHRC_} "alias nx-cd-en='cd /etc/nginx/sites-enabled/'"
 
 source ~/.bashrc
+
+cd ${_RETURN_PATH_}
